cc3aaced7655a31bd6ea3d901c6ec27ec9ea3a34
taheralala <ta7ermo2002@gmail.com>
2025-10-02 23:25:19 +0300

    refactor: refactor flags/file.go
    - Change some struct field name like User to name and Args to Filename 
    - Change default Usage msg. 
    - Change CheckArgs name to ValidateFilename.
    - Change CheckFlag name to ValidateFlags.
    - Update CheckFlag (ValidateFlags) logic to validate each flag individually.
    - Add IsOnlyOneFalgSet func to check if one flag or not all.    
    - Add IsAllFlagNotSet
    - Modify format of String function.

--- flags/flags.go
+++ flags/flags.go
@@ -6,21 +6,20 @@ import (
 	"flag"
 	"fmt"
 	"os"
-	"strings"
 )
 
 type FlagConfig struct {
-	User   string
-	Repo   string
-	Branch string
-	Args   []string
+	Name     string
+	Repo     string
+	Branch   string
+	Filename []string
 }
 
 func Load() (*FlagConfig, error) {
 	fcfg := &FlagConfig{}
 
-	flag.StringVar(&fcfg.User, "name", "", "Github user name")
-	flag.StringVar(&fcfg.User, "n", "", "Github user name")
+	flag.StringVar(&fcfg.Name, "name", "", "Github user name")
+	flag.StringVar(&fcfg.Name, "n", "", "Github user name")
 
 	flag.StringVar(&fcfg.Repo, "repo", "", "Github repo name")
 	flag.StringVar(&fcfg.Repo, "r", "", "Github repo name")
@@ -29,61 +28,68 @@ func Load() (*FlagConfig, error) {
 	flag.StringVar(&fcfg.Branch, "branch", "", "Github branch name")
 
 	flag.Usage = func() {
-		fmt.Fprintf(os.Stderr, "Usage: myapp [options]")
-		fmt.Fprintf(os.Stderr, "\nRequired flags:")
-		fmt.Fprintf(os.Stderr, "  -n, -name     GitHub user name")
-		fmt.Fprintf(os.Stderr, "  -r, -repo     GitHub repo name")
-		fmt.Fprintf(os.Stderr, "  -b, -branch   GitHub branch name")
-		fmt.Fprintf(os.Stderr, "\nOptions:")
-		flag.PrintDefaults()
+		fmt.Fprintf(os.Stderr, "\nUsage: ghget [options]")
+		fmt.Fprintf(os.Stderr, "\nRequired flags:\n")
+		fmt.Fprintf(os.Stderr, "  -n, -name     GitHub user name\n")
+		fmt.Fprintf(os.Stderr, "  -r, -repo     GitHub repo name\n")
+		fmt.Fprintf(os.Stderr, "  -b, -branch   GitHub branch name\n")
+		fmt.Fprintf(os.Stderr, "\n\nOptions:\n")
 	}
 
 	flag.Parse()
-	fcfg.Args = flag.Args()
 
-	if err := fcfg.CheckFlag(); err != nil {
-		return nil, err
-	}
+	fcfg.Filename = flag.Args()
 
-	if err := fcfg.CheckArgs(); err != nil {
+	if err := fcfg.ValidateFilename(); err != nil {
 		return nil, err
 	}
 
 	return fcfg, nil
 }
 
-func (cfg *FlagConfig) CheckArgs() error {
-	if len(cfg.Args) == 0 {
+func (cfg *FlagConfig) ValidateFilename() error {
+	if len(cfg.Filename) == 0 {
 		return errors.New("missing file name")
 	}
+
+	if len(cfg.Filename) > 1 {
+		return errors.New("only accept one file for right now")
+	}
+
 	return nil
 }
 
-func (cfg *FlagConfig) CheckFlag() error {
-	var missing []string
-
-	if cfg.User == "" {
-		missing = append(missing, "user")
+func (cfg *FlagConfig) ValidateFlags() error {
+	if cfg.Name == "" {
+		return errors.New("missing name flag")
 	}
+
 	if cfg.Branch == "" {
-		missing = append(missing, "branch")
-	}
-	if cfg.Repo == "" {
-		missing = append(missing, "repo")
+		return errors.New("missing branch flag")
 	}
 
-	if len(missing) > 0 {
-		return errors.New("missing required flags: " + strings.Join(missing, ", "))
+	if cfg.Repo == "" {
+		return errors.New("missing repo flag")
 	}
+
 	return nil
+}
+
+func (cfg *FlagConfig) IsOnlyOneFalgSet() bool {
+	return cfg.Name != "" || cfg.Branch != "" || cfg.Repo != ""
+}
+
+func (cfg *FlagConfig) IsAllFalgNotSet() bool {
+	return cfg.Name != "" && cfg.Branch != "" && cfg.Repo != ""
 }
 
 func (cfg *FlagConfig) String() string {
-	format := `Username: %s\nRepo: %s\nBranch: %s\nArgs:%v\n`
+	format := "Username: %s\nRepo: %s\nBranch: %s\nFilename: %v\n"
 	return fmt.Sprintf(format,
-		cfg.User,
+		cfg.Name,
 		cfg.Repo,
 		cfg.Branch,
-		cfg.Args,
+		cfg.Filename,
 	)
 }
+
