497e1a4f1b2b7054264261b0ddc3bb992d26f13e
taheralala <ta7ermo2002@gmail.com>
2025-10-07 00:44:40 +0300

    Add: add client and downloader
    - Add client type to add fetch file and pass it to downloader to
      download it.
    - A client.FetchFile return body of response. 
    - A client.checkStatus check statusCode if request.
    
    - downloader contain some funcs
    	- DownloadFromConfig take data from file config and filenames and
    	  called DownloadFile to download files.
    	- DownloadFile file in cwd. 
    	- BuildURLs return array of url to fetch data from.
    	- parseFilenames to download files inside directroy in github for
    	  example: `go/mise.toml`.

---
+++ client/client.go
@@ -0,0 +1,53 @@
+package client
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+)
+
+type Client struct {
+	httpClient *http.Client
+}
+
+func New(timeout time.Duration) *Client {
+	return &Client{
+		httpClient: &http.Client{
+			Timeout: timeout,
+		},
+	}
+}
+
+func (c *Client) FetchFile(url string) (io.ReadCloser, error) {
+	resp, err := c.httpClient.Get(url)
+	if err != nil {
+		return nil, fmt.Errorf("request failed: %w", err)
+	}
+
+	if err := checkStatus(resp); err != nil {
+		resp.Body.Close()
+		return nil, err
+	}
+
+	return resp.Body, nil
+}
+
+func checkStatus(resp *http.Response) error {
+	if resp.StatusCode >= 200 && resp.StatusCode < 300 {
+		return nil
+	}
+
+	switch resp.StatusCode {
+	case http.StatusNotFound:
+		return fmt.Errorf("file not found (404) - check user, repo, branch or file name")
+	case http.StatusForbidden:
+		return fmt.Errorf("access forbidden (403) - check repository visibility")
+	case http.StatusUnauthorized:
+		return fmt.Errorf("unauthorized (401) - authentication required")
+	case http.StatusTooManyRequests:
+		return fmt.Errorf("rate limited (429) - try again later")
+	default:
+		return fmt.Errorf("request failed with status: %s", resp.Status)
+	}
+}
---
+++ downloader/downloader.go
@@ -0,0 +1,85 @@
+package downloader
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/ta7eralla/ghget/client"
+	"github.com/ta7eralla/ghget/config"
+)
+
+type Downloader struct {
+	client *client.Client
+}
+
+func New() *Downloader {
+	return &Downloader{
+		client: client.New(2 * time.Minute),
+	}
+}
+
+func (d *Downloader) DownloadFromConfig(cfg *config.Config, filenames []string) error {
+	urls := d.BuildURLs(cfg, filenames)
+
+	filenames = d.parseFilenames(filenames)
+
+	for i, url := range urls {
+		if err := d.DownloadFile(url, filenames[i]); err != nil {
+			return fmt.Errorf("failed download %s: %w", filenames[i], err)
+		}
+		fmt.Printf("Downloaded %s\n", filenames[i])
+	}
+
+	return nil
+}
+
+func (d *Downloader) DownloadFile(url, filename string) error {
+	body, err := d.client.FetchFile(url)
+	if err != nil {
+		return err
+	}
+	defer body.Close()
+
+	file, err := os.Create(filename)
+	if err != nil {
+		return fmt.Errorf("creating file: %w", err)
+	}
+	defer file.Close()
+
+	if _, err := io.Copy(file, body); err != nil {
+		return fmt.Errorf("%w", err)
+	}
+
+	return nil
+}
+
+func (d *Downloader) BuildURLs(cfg *config.Config, filenames []string) []string {
+	publicURL := "https://raw.githubusercontent.com/%s/%s/refs/heads/%s/%s"
+
+	urls := make([]string, 0, len(filenames))
+	for _, filename := range filenames {
+		urls = append(urls, fmt.Sprintf(publicURL, cfg.Name, cfg.Repo, cfg.Branch, filename))
+	}
+	return urls
+}
+
+func (d *Downloader) parseFilenames(filenames []string) []string {
+	var files []string
+
+	for _, file := range filenames {
+		if file == "" {
+			continue
+		}
+		file = strings.TrimSuffix(file, "/")
+		if idx := strings.LastIndex(file, "/"); idx != -1 {
+			files = append(files, file[idx+1:])
+		} else {
+			files = append(files, file)
+		}
+
+	}
+	return files
+}
